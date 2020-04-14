6715d24ed3247cd85d95114ac281e24a66cb8327
Marcus Hansson <machalvan@hotmail.com>
2020-04-14 07:00:17 +0200

    Add more statistics

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -9,6 +9,9 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const all = good + neutral + bad
+  const average = (good - bad) / all
+  const positive = good / all * 100
 
   return (
     <div>
@@ -18,9 +21,12 @@ const App = () => {
       <Button text="bad" onClick={() => setBad(bad + 1)} />
 
       <Title text="statistics" />
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
+      <div>good {good}</div>
+      <div>neutral {neutral}</div>
+      <div>bad {bad}</div>
+      <div>all {all}</div>
+      <div>average {average}</div>
+      <div>positive {positive} %</div>
     </div>
   )
 }
