346bc35f92042bdc46c7234696805f0312b502b2
Marcus Hansson <machalvan@hotmail.com>
2020-04-14 06:49:40 +0200

    Handle button click

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,20 +1,23 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Title = ({text}) => <h1>{text}</h1>
+
+const Button = ({text, onClick}) => <button onClick={onClick}>{text}</button>
+
 const App = () => {
-  // save clicks of each button to own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
   return (
     <div>
-      <h1>give feedback</h1>
-      <button>good</button>
-      <button>neutral</button>
-      <button>bad</button>
+      <Title text="give feedback" />
+      <Button text="good" onClick={() => setGood(good + 1)} />
+      <Button text="neutral" onClick={() => setNeutral(neutral + 1)} />
+      <Button text="bad" onClick={() => setBad(bad + 1)} />
 
-      <h1>statistics</h1>
+      <Title text="statistics" />
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
