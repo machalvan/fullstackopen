e42a80dc687e2a6a31aea621562755e6debb2e8d
Marcus Hansson <machalvan@hotmail.com>
2020-04-18 08:30:07 +0200

    Allow multiple cources

--- part2/course-contents/src/index.js
+++ part2/course-contents/src/index.js
@@ -16,7 +16,7 @@ const Total = ({parts}) => {
     .map(el => el.exercises)
     .reduce((acc, cur) => acc + cur)
 
-  return <p>Number of exercises {exerciseSum}</p>
+  return <b>total of {exerciseSum} exercises</b>
 }
 
 const Course = ({name, parts}) => (
@@ -28,31 +28,54 @@ const Course = ({name, parts}) => (
 )
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
   return (
     <div>
-      <Course {...course} />
+      {courses.map(course => <Course {...course} />)}
     </div>
   )
 }
