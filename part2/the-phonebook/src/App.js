b32c41a495ce056a0f871fd0cb972f0937658929
Marcus Hansson <machalvan@hotmail.com>
2020-04-30 08:22:06 +0200

    Store persons in json server database

--- part2/the-phonebook/src/App.js
+++ part2/the-phonebook/src/App.js
@@ -1,18 +1,10 @@
 import React, {useEffect, useState} from 'react'
-import axios from "axios";
+import {personService} from "./services/personService";
 
-const PersonForm = ({addPerson, handleNameChange, handleNumberChange}) => (
-  <form onSubmit={addPerson}>
-    <Input label="name: " onChange={handleNameChange} />
-    <Input label="number: " onChange={handleNumberChange} />
-    <SubmitButton text="add" />
-  </form>
-)
-
-const Input = ({label, onChange}) => (
+const Input = ({label, ...props}) => (
   <div>
     {label}
-    <input onChange={onChange} />
+    <input {...props} />
   </div>
 )
 
@@ -22,7 +14,15 @@ const SubmitButton = ({text}) => (
   </div>
 )
 
-const Persons = ({persons}) => persons.map(p => <div key={p.name}>{p.name} {p.number}</div>)
+const PersonForm = ({addPerson, handleNameChange, handleNumberChange, newName, newNumber}) => (
+  <form onSubmit={addPerson}>
+    <Input label="name: " value={newName} onChange={handleNameChange} />
+    <Input label="number: " value={newNumber} onChange={handleNumberChange} />
+    <SubmitButton text="add" />
+  </form>
+)
+
+const Persons = ({persons}) => persons.map(({name, number}) => <div key={name}>{name} {number}</div>)
 
 const App = () => {
   const [persons, setPersons] = useState([])
@@ -34,23 +34,29 @@ const App = () => {
 
   const addPerson = (event) => {
     event.preventDefault()
-    if (!persons.find(person => person.name === newName)) {
-      setPersons(persons.concat({name: newName, number: newNumber}));
+    if (!persons.find(({name}) => name === newName)) {
+      personService
+        .create({name: newName, number: newNumber})
+        .then(newPerson =>  {
+          setPersons(persons.concat(newPerson));
+          setNewName('')
+          setNewNumber('')
+        })
     } else {
       alert(`${newName} is already added to phonebook`)
     }
   }
 
   useEffect(() => {
-    axios
-      .get('http://localhost:3001/persons')
-      .then(res => setPersons(res.data))
+    personService
+      .getAll()
+      .then(initialPersons => setPersons(initialPersons))
   }, [])
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <PersonForm {...{addPerson, handleNameChange, handleNumberChange}} />
+      <PersonForm {...{addPerson, handleNameChange, handleNumberChange, newName, newNumber}} />
       <h2>Numbers</h2>
       <Persons persons={persons} />
     </div>
