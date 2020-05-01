92a0954cfb1a9609bb194129342920ea07c0c0a2
Marcus Hansson <machalvan@hotmail.com>
2020-05-01 10:14:23 +0200

    Add styling

--- part2/the-phonebook/src/App.js
+++ part2/the-phonebook/src/App.js
@@ -3,14 +3,7 @@ import {personService} from "./services/personService";
 
 const Input = ({label, ...props}) => (
   <div>
-    {label}
-    <input {...props} />
-  </div>
-)
-
-const SubmitButton = ({text}) => (
-  <div>
-    <button type="submit">{text}</button>
+    {label} <input {...props} />
   </div>
 )
 
@@ -18,20 +11,25 @@ const PersonForm = ({handleSubmit, input1, input2}) => (
   <form onSubmit={handleSubmit}>
     <Input {...input1} />
     <Input {...input2} />
-    <SubmitButton text="add" />
+    <button type="submit">add</button>
   </form>
 )
 
-const Persons = ({persons, deletePerson}) => persons.map(person => (
+const Person = ({person, deletePerson}) => (
   <div key={person.name}>
     {person.name} {person.number} <button onClick={() => deletePerson(person)}>delete</button>
   </div>
-))
+)
+
+const Notification = ({message, type}) => <div className={`button ${type}`}>{message}</div>
 
 const App = () => {
   const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [success, setSuccess] = useState('')
+  const [error, setError] = useState('')
+
   const handleNameChange = (event) => setNewName(event.target.value)
   const handleNumberChange = (event) => setNewNumber(event.target.value)
   const input1 = { label: "name: ", value: newName, onChange: handleNameChange}
@@ -50,9 +48,11 @@ const App = () => {
         .then(newPerson => {
           setPersons(persons.concat(newPerson));
           resetFields()
+          showNotification(`Added ${newName}`, "success")
         })
     } else {
-      alert(`${newName} is already added to phonebook`)
+      setError(`${newName} is already added to phonebook`)
+      showNotification(`${newName} is already added to phonebook`, "error")
     }
   }
 
@@ -60,13 +60,28 @@ const App = () => {
     if (window.confirm(`Delete ${name}?`)) {
       personService
         .remove(id)
-        .then(() => setPersons(persons.filter(p => p.id !== id)))
+        .then(() => {
+          setPersons(persons.filter(p => p.id !== id))
+          showNotification(`Successfully deleted ${newName}`, "success")
+        })
     }
   }
 
   const handleSubmit = (event) => {
     event.preventDefault()
     addPerson()
+  }
+
+  const showNotification = (message, type) => {
+    const timeout = 5000
+
+    if (type === "success") {
+      setSuccess(message)
+      setTimeout(() => setSuccess(null), timeout);
+    } else {
+      setError(message)
+      setTimeout(() => setError(null), timeout);
+    }
   }
 
   useEffect(() => {
@@ -78,9 +93,13 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      {success && <Notification message={success} type="success" />}
+      {error && <Notification message={error} type="error" />}
       <PersonForm {...{handleSubmit, input1, input2}} />
       <h2>Numbers</h2>
-      <Persons {...{persons, deletePerson}} />
+      {persons.map(person => (
+        <Person key={person.name} {...{person, deletePerson}} />
+      ))}
     </div>
   )
 }
